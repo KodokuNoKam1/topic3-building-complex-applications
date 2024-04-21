070be7df41a997e6329614393282ca3ed6dadde0
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2024-04-21 18:26:50 +0300

    ProfileInfo

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.topic3.android.reddit.R
 
 import com.topic3.android.reddit.theme.RedditThemeSettings
@@ -65,6 +67,7 @@ private fun AppDrawerHeader() {
     )
     Text(text = stringResource(id = R.string.default_username),
       color = MaterialTheme.colors.primaryVariant)
+    ProfileInfo()
   }
   Divider(
     color = MaterialTheme.colors.onSurface.copy(alpha = .2f),
@@ -73,132 +76,165 @@ private fun AppDrawerHeader() {
     )
   )
 }
-@Composable
-fun ProfileInfo() {
-  //TODO add your code here
-}
-@Composable
-private fun ProfileInfoItem(
-  iconAsset: ImageVector,
-  amountResourceId: Int,
-  textResourceId: Int,
-  modifier: Modifier
-) {
-  //TODO add your code here
-  val colors = MaterialTheme.colors
 
-  ConstraintLayout(modifier = modifier) {
-    val (iconRef, amountRef, titleRef) = createRefs()
-    val itemModifier = Modifier
+@Composable
+fun ProfileInfo(modifier: Modifier = Modifier) {
+    ConstraintLayout(
+      modifier = modifier
+        .fillMaxWidth()
+        .padding(top = 16.dp)
+    ) {
+      val (karmaItem, divider, ageItem) = createRefs()
+      val colors = MaterialTheme.colors
 
-    Icon(
-      contentDescription = stringResource(id = textResourceId),
-      imageVector = iconAsset,
-      tint = Color.Blue,
-      modifier = itemModifier
-        .constrainAs(iconRef) {
+      ProfileInfoItem(
+        Icons.Filled.Star,
+        R.string.default_karma_amount,
+        R.string.karma,
+        modifier = modifier.constrainAs(karmaItem){
           centerVerticallyTo(parent)
           start.linkTo(parent.start)
         }
-        .padding(start = 16.dp)
-    )
-    Text(
-      text = stringResource(amountResourceId),
-      color = colors.primaryVariant,
-      fontSize = 10.sp,
-      modifier = itemModifier
-        .padding(start = 8.dp)
-        .constrainAs(amountRef) {
-          top.linkTo(iconRef.top)
-          start.linkTo(iconRef.end)
-          bottom.linkTo(titleRef.top)
+      )
+      Divider(
+        modifier = modifier
+          .width(1.dp)
+          .constrainAs(divider) {
+            centerVerticallyTo(karmaItem)
+            centerHorizontallyTo(parent)
+            height = Dimension.fillToConstraints
+          },
+        color = colors.onSurface.copy(alpha = .2f)
+      )
+      ProfileInfoItem(Icons.Filled.ShoppingCart,
+        R.string.default_reddit_age_amount,
+        R.string.reddit_age,
+        modifier = modifier.constrainAs(ageItem){
+          start.linkTo(divider.end)
+          centerVerticallyTo(parent)
         }
-    )
-    Text(
-      text = stringResource(textResourceId),
-      color = Color.Gray,
-      fontSize = 10.sp,
-      modifier = itemModifier
-        .padding(start = 8.dp)
-        .constrainAs(titleRef){
-          top.linkTo(amountRef.bottom)
-          start.linkTo(iconRef.end)
-          bottom.linkTo(iconRef.bottom)
-        }
-    )
-  }
-}
-
-/**
- * Представляет действия drawer приложения:
- * * экранная навигация
- * * светлый/темный режим приложения
- */
-@Composable
-private fun AppDrawerBody(closeDrawerAction: () -> Unit) {
-  //TODO add your code here
-}
-/**
- * Представляет компонент в панели приложений, который пользователь может использовать для смены экрана.
- */
-@Composable
-private fun ScreenNavigationButton(
-  icon: ImageVector,
-  label: String,
-  onClickAction: () -> Unit,
-  modifier: Modifier = Modifier
-) {
-  val colors = MaterialTheme.colors
-  val surfaceModifier = modifier
-    .padding(start = 8.dp, top = 8.dp, end = 8.dp)
-    .fillMaxWidth()
-  Surface(
-    modifier = surfaceModifier,
-    color = colors.surface,
-    shape = MaterialTheme.shapes.small
-  ) {
-    TextButton(
-      onClick = onClickAction,
-      modifier = Modifier.fillMaxWidth()
-    ) {
-      Row(
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
-      ) {
-        Image(
-          imageVector = icon,
-          colorFilter = ColorFilter.tint(Color.Gray),
-          contentDescription = label
-        )
-        Spacer(Modifier.width(16.dp))
-        Text(
-          fontSize = 10.sp,
-          text = label,
-          style = MaterialTheme.typography.body2,
-          color = colors.primaryVariant
-        )
-      }
+      )
     }
   }
-}
-/**
- * Представляет компонент настройки в панели приложений.
- */
-@Composable
-private fun AppDrawerFooter(modifier: Modifier = Modifier) {
-  //TODO add your code here
-}
-private fun changeTheme() {
-  RedditThemeSettings.isInDarkTheme.value = RedditThemeSettings.isInDarkTheme.value.not()
-}
-@Preview
-@Composable
-private fun ProfileInfoItemPreview() {
-  ProfileInfoItem(
-    Icons.Filled.ShoppingCart,
-    R.string.default_reddit_age_amount,
-    R.string.reddit_age,
-    Modifier
-  )
-}
+
+  @Composable
+  private fun ProfileInfoItem(
+    iconAsset: ImageVector,
+    amountResourceId: Int,
+    textResourceId: Int,
+    modifier: Modifier
+  ) {
+    val colors = MaterialTheme.colors
+    ConstraintLayout(modifier = modifier) {
+      val (iconRef, amountRef, titleRef) = createRefs()
+      val itemModifier = Modifier
+      Icon(
+        contentDescription = stringResource(id = textResourceId),
+        imageVector = iconAsset,
+        tint = Color.Blue,
+        modifier = itemModifier
+          .constrainAs(iconRef) {
+            centerVerticallyTo(parent)
+            start.linkTo(parent.start)
+          }
+          .padding(start = 16.dp)
+      )
+      Text(
+        text = stringResource(amountResourceId),
+        color = colors.primaryVariant,
+        fontSize = 10.sp,
+        modifier = itemModifier
+          .padding(start = 8.dp)
+          .constrainAs(amountRef) {
+            top.linkTo(iconRef.top)
+            start.linkTo(iconRef.end)
+            bottom.linkTo(titleRef.top)
+          }
+      )
+      Text(
+        text = stringResource(textResourceId),
+        color = Color.Gray,
+        fontSize = 10.sp,
+        modifier = itemModifier
+          .padding(start = 8.dp)
+          .constrainAs(titleRef) {
+            top.linkTo(amountRef.bottom)
+            start.linkTo(iconRef.end)
+            bottom.linkTo(iconRef.bottom)
+          }
+            )
+          }
+    }
+    /**
+     * Представляет действия drawer приложения:
+     * * экранная навигация
+     * * светлый/темный режим приложения
+     */
+    @Composable
+    private fun AppDrawerBody(closeDrawerAction: () -> Unit) {
+      //TODO add your code here
+    }
+    /**
+     * Представляет компонент в панели приложений, который пользователь может использовать для смены экрана.
+     */
+    @Composable
+    private fun ScreenNavigationButton(
+      icon: ImageVector,
+      label: String,
+      onClickAction: () -> Unit,
+      modifier: Modifier = Modifier
+    ) {
+      val colors = MaterialTheme.colors
+      val surfaceModifier = modifier
+        .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+        .fillMaxWidth()
+      Surface(
+        modifier = surfaceModifier,
+        color = colors.surface,
+        shape = MaterialTheme.shapes.small
+      ) {
+        TextButton(
+          onClick = onClickAction,
+          modifier = Modifier.fillMaxWidth()
+        ) {
+          Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+          ) {
+            Image(
+              imageVector = icon,
+              colorFilter = ColorFilter.tint(Color.Gray),
+              contentDescription = label
+            )
+            Spacer(Modifier.width(16.dp))
+            Text(
+              fontSize = 10.sp,
+              text = label,
+              style = MaterialTheme.typography.body2,
+              color = colors.primaryVariant
+            )
+          }
+        }
+      }
+    }
+    /**
+     * Представляет компонент настройки в панели приложений.
+     */
+    @Composable
+    private fun AppDrawerFooter(modifier: Modifier = Modifier) {
+      //TODO add your code here
+    }
+    private fun changeTheme() {
+      RedditThemeSettings.isInDarkTheme.value = RedditThemeSettings.isInDarkTheme.value.not()
+    }
+    @Preview
+    @Composable
+    private fun ProfileInfoItemPreview() {
+      ProfileInfoItem(
+        Icons.Filled.ShoppingCart,
+        R.string.default_reddit_age_amount,
+        R.string.reddit_age,
+        Modifier
+      )
+    }
